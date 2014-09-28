020f81a705b545b5c446de4a2b0fa2d42b9fc773
Muzzhum <mgnusk@gmail.com>
2014-09-28 19:43:09 +0200

    Working on problem 5 and a new function!
    I have an infinitie loop in problem 5. No idea why.

--- main.cpp
+++ main.cpp
@@ -197,33 +197,47 @@ int largestPalindromeProduct(int numOfCiphers) // Project Euler problem 4 unfini
     return 0;
 }
 
+bool dividesByLessThan(int topLim, int number)
+{
+    bool isDivisible = true;
+    for (int iii = 0 ; iii <= topLim ; iii++)
+    {
+        if ((topLim - iii) % number)
+            isDivisible = false;
+        cout << topLim << " " << iii << " " << number << " " << isDivisible << endl;
+    }
+    return isDivisible;
+}
+
 int smallestMultiple(int topLim) // Project Euler problem 5 unfinished
 {
-    int smallest = 1;
-    for (int iii = 2 ; iii <= topLim ;)
+    int smallestNum = 2;
+    for (int iii = 2 ; iii <= topLim ; )
     {
-        if (iii % smallest)
+        if (dividesByLessThan(topLim, smallestNum))
             iii++;
         else
-            smallest += iii;
+            smallestNum++;
+        cout << smallestNum << " " << iii << endl;
     }
+
     // This for loop is just an error checker. It prints ERROR if number is
     // not evenly divisible by counter.
     for (int counter = 2 ; counter <= topLim ; counter++)
     {
-        if (smallest % counter)
+        if (smallestNum % counter)
         {
             cout << "ERROR" << endl;
             break;
         }
     }
-    cout << smallest << endl;
+    cout << smallestNum << endl;
     return 0;
 }
 
 int main()
 {
-    smallestMultiple(4);
+    smallestMultiple(3);
 
     return 0;
 }
